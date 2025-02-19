042fbaeb1e3e09f2d1e194c39e068b4de4223bba
Ronan Soares <ronanp2@hotmail.com>
2025-02-19 10:27:18 -0300

    Cria Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,21 @@
 package application;
 
+import java.util.Date;
+
+import com.mysql.fabric.xmlrpc.base.Data;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dep = new Department(1, "Livros");
-		
+		Department dep = new Department(1, "Livros");		
 		System.out.println(dep);
+		
+		Seller seller = new Seller(1, "Maria", "maria@hotmail.com", new Date(), 3000.0, dep);
+		System.out.println(seller);
 
 	}
-
 }
